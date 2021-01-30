d5563bfc62de757f39f9b15fe7f9574fd64096a4
Barracuda-AG <Nelson_73>
2021-01-30 18:56:23 +0200

    second,third tasks completed

--- src/main/java/ua/gorbatov/datetime_homework/DateTimeMethods.java
+++ src/main/java/ua/gorbatov/datetime_homework/DateTimeMethods.java
@@ -1,8 +1,6 @@
 package ua.gorbatov.datetime_homework;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.Month;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,17 +13,54 @@ public class DateTimeMethods {
      * “Jul 2013”, список упорядочен по возрастанию дат.
      * @return List<String> with results
      */
-    List<String> fridays13(){
+    List<String> fridays13() {
         List<String> result = new ArrayList<>();
         LocalDate now = LocalDate.now();
         LocalDate start = LocalDate.of(2000, Month.JANUARY, 1);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM yyyy", Locale.ENGLISH);
 
-       while(start.isBefore(now)){
-            if(start.getDayOfWeek() == DayOfWeek.FRIDAY && start.getDayOfMonth() == 13) {
+        while (start.isBefore(now)) {
+            if (start.getDayOfWeek() == DayOfWeek.FRIDAY && start.getDayOfMonth() == 13) {
                 result.add(formatter.format(start));
             }
             start = start.plusDays(1);
+        }
+        return result;
+    }
+        /**
+         * 2. Список месяцев, которые заканчиваются в воскресенье. От 2000 года до
+         * сегодня.
+         * Метод: List<YearMonth> endOnSundays() , список упорядочен по возрастанию
+         * дат.
+         */
+        List<YearMonth> endOnSundays(){
+            List<YearMonth> result = new ArrayList<>();
+            YearMonth now = YearMonth.now();
+            YearMonth start = YearMonth.of(2000, Month.JANUARY);
+
+            while(start.isBefore(now)){
+                if(start.atEndOfMonth().getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+                    result.add(start);
+                }
+                start = start.plusMonths(1);
+            }
+            return result;
+        }
+    /**
+     * 3. Список годов, когда заданный день рождения попадает на субботу. От даты
+     * рождения до сегодня.
+     * Метод: List<Year> birthdaysOnSaturdays(LocalDate birthday) , список годов
+     * упорядочен по возрастанию.
+     */
+    List<Year> birthdaysOnSaturdays(LocalDate birthday){
+        List<Year> result = new ArrayList<>();
+        LocalDate start = birthday;
+        LocalDate now = LocalDate.now();
+        while (start.isBefore(now)){
+            if(start.getDayOfWeek().equals(DayOfWeek.SATURDAY)){
+                result.add(Year.of(start.getYear()));
+            }
+            start = start.plusYears(1);
         }
         return result;
     }
--- src/test/java/ua/gorbatov/datetime_homework/DateTimeMethodsTest.java
+++ src/test/java/ua/gorbatov/datetime_homework/DateTimeMethodsTest.java
@@ -3,6 +3,9 @@ package ua.gorbatov.datetime_homework;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +14,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class DateTimeMethodsTest {
     private DateTimeMethods dateTimeMethods;
     private List<String> fridays13Expected;
+    private List<YearMonth> endOnSundaysExpected;
+    private List<Year> birthdaysOnSaturdaysExpected;
 
     @BeforeEach
     void setUp(){
+        dateTimeMethods = new DateTimeMethods();
         fridays13Expected =  new ArrayList<>();
         fridays13Expected.add("Oct 2000");
         fridays13Expected.add("Apr 2001");
@@ -53,13 +59,69 @@ class DateTimeMethodsTest {
         fridays13Expected.add("Mar 2020");
         fridays13Expected.add("Nov 2020");
 
+        endOnSundaysExpected = new ArrayList<>();
+        endOnSundaysExpected.add(YearMonth.of(2000, 4));
+        endOnSundaysExpected.add(YearMonth.of(2000, 12));
+        endOnSundaysExpected.add(YearMonth.of(2001, 9));
+        endOnSundaysExpected.add(YearMonth.of(2002, 3));
+        endOnSundaysExpected.add(YearMonth.of(2002, 6));
+        endOnSundaysExpected.add(YearMonth.of(2003, 8));
+        endOnSundaysExpected.add(YearMonth.of(2003, 11));
+        endOnSundaysExpected.add(YearMonth.of(2004, 2));
+        endOnSundaysExpected.add(YearMonth.of(2004, 10));
+        endOnSundaysExpected.add(YearMonth.of(2005, 7));
+        endOnSundaysExpected.add(YearMonth.of(2006, 4));
+        endOnSundaysExpected.add(YearMonth.of(2006, 12));
+        endOnSundaysExpected.add(YearMonth.of(2007, 9));
+        endOnSundaysExpected.add(YearMonth.of(2008, 8));
+        endOnSundaysExpected.add(YearMonth.of(2008, 11));
+        endOnSundaysExpected.add(YearMonth.of(2009, 5));
+        endOnSundaysExpected.add(YearMonth.of(2010, 1));
+        endOnSundaysExpected.add(YearMonth.of(2010, 2));
+        endOnSundaysExpected.add(YearMonth.of(2010, 10));
+        endOnSundaysExpected.add(YearMonth.of(2011, 7));
+        endOnSundaysExpected.add(YearMonth.of(2012, 9));
+        endOnSundaysExpected.add(YearMonth.of(2013, 3));
+        endOnSundaysExpected.add(YearMonth.of(2013, 6));
+        endOnSundaysExpected.add(YearMonth.of(2014, 8));
+        endOnSundaysExpected.add(YearMonth.of(2014, 11));
+        endOnSundaysExpected.add(YearMonth.of(2015, 5));
+        endOnSundaysExpected.add(YearMonth.of(2016, 1));
+        endOnSundaysExpected.add(YearMonth.of(2016, 7));
+        endOnSundaysExpected.add(YearMonth.of(2017, 4));
+        endOnSundaysExpected.add(YearMonth.of(2017, 12));
+        endOnSundaysExpected.add(YearMonth.of(2018, 9));
+        endOnSundaysExpected.add(YearMonth.of(2019, 3));
+        endOnSundaysExpected.add(YearMonth.of(2019, 6));
+        endOnSundaysExpected.add(YearMonth.of(2020, 5));
+       // endOnSundaysExpected.add(YearMonth.of(2021, 1));
+
+        birthdaysOnSaturdaysExpected = new ArrayList<>();
+        birthdaysOnSaturdaysExpected.add(Year.of(1993));
+        birthdaysOnSaturdaysExpected.add(Year.of(1999));
+        birthdaysOnSaturdaysExpected.add(Year.of(2004));
+        birthdaysOnSaturdaysExpected.add(Year.of(2010));
 
     }
     @Test
     void fridays13() {
-        dateTimeMethods = new DateTimeMethods();
         List<String> actual = dateTimeMethods.fridays13();
         assertEquals(fridays13Expected.size(), actual.size());
         assertTrue(actual.containsAll(fridays13Expected));
+    }
+
+    @Test
+    void endOnSundays() {
+        List<YearMonth> actual = dateTimeMethods.endOnSundays();
+        assertEquals(endOnSundaysExpected.size(), actual.size());
+        assertTrue(actual.containsAll(endOnSundaysExpected));
+    }
+
+    @Test
+    void birthdaysOnSaturdays() {
+        LocalDate birthDay = LocalDate.of(1990, 6, 12);
+        List<Year> actual = dateTimeMethods.birthdaysOnSaturdays(birthDay);
+        assertTrue(actual.containsAll(birthdaysOnSaturdaysExpected));
+
     }
 }
